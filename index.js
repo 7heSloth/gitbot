88c988b68c5a3d250885abce3e9b95ba12c76f8e
Justin <justinjong94@gmail.com>
2019-02-22 20:33:40 +0800

    second commit

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
-console.log("hello world");
+const Telegraf = require('telegraf');
 const API_KEY = "770360803:AAGcVXhBOfvlSzIM-PkXZRa1lyMQMnzIxVI";
+
+
+ 
+const bot = new Telegraf(API_KEY);
+
+//modern syntax with default return
+
+//bot.start((ctx) => ctx.reply('Welcome!'));
+const helphandler = context => context.reply("Shaun is a bitch!");
+//bot.on('sticker', (ctx) => ctx.reply('👍'));
+//bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.help(helphandler);
+
+bot.command("hello", context => context.reply("heyyo"));
+bot.launch();
+
+
+
+
+/*
+function greet(name) {
+    return "hello" + name;
+}
+
+
+const greet = function (name) {
+    return "hello" + name;
+}
+
+//modern syntax
+const greetagain = (name) => {
+    return "hello" + name;
+    const greet2 = name => hello + "name "
+}*/
